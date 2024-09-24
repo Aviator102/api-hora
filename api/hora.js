933d430dccbb63299418c1f4b10f994c77bb246a
Sonhador Brasil <magazinegravatai@gmail.com>
2024-09-23 22:38:39 -0300

    Update hora.js

--- api/hora.js
+++ api/hora.js
@@ -1,10 +1,17 @@
 export default function handler(req, res) {
     const agora = new Date();
+    
+    // Formatar a hora
     const horaAtual = agora.toLocaleTimeString('pt-BR', { timeZone: 'America/Sao_Paulo' });
-    const dataAtual = agora.toLocaleDateString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+    // Formatar a data para YYYY-MM-DD
+    const dataAtual = agora.toISOString().split('T')[0]; // YYYY-MM-DD
 
-    res.status(200).json({
+    // Formatar a resposta para incluir ano
+    const resposta = {
+        ano: agora.getFullYear(),
         hora: horaAtual,
         data: dataAtual
-    });
+    };
+
+    res.status(200).json(resposta);
 }
